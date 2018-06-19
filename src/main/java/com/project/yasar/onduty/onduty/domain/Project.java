a17c4c9bf07346779d3e8605361bc7e1f43c8d48
ASUS1 <melisaliyazicioglu@hotmail.com>
2018-06-19 16:42:57 +0300

    project tamamlandı

--- src/main/java/com/project/yasar/onduty/onduty/domain/Project.java
+++ src/main/java/com/project/yasar/onduty/onduty/domain/Project.java
@@ -21,6 +21,53 @@ public class Project {
 	    private ProjectState projectstate;
 	 @Enumerated(EnumType.ORDINAL)
 	    private StateType statetype;
+	public Project(String name, Date starttime, Date enddate, ProjectState projectstate, StateType statetype) {
+		super();
+		this.name = name;
+		this.starttime = starttime;
+		this.enddate = enddate;
+		this.projectstate = projectstate;
+		this.statetype = statetype;
+	}
+	public Project() {
+		
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getStarttime() {
+		return starttime;
+	}
+	public void setStarttime(Date starttime) {
+		this.starttime = starttime;
+	}
+	public Date getEnddate() {
+		return enddate;
+	}
+	public void setEnddate(Date enddate) {
+		this.enddate = enddate;
+	}
+	public ProjectState getProjectstate() {
+		return projectstate;
+	}
+	public void setProjectstate(ProjectState projectstate) {
+		this.projectstate = projectstate;
+	}
+	public StateType getStatetype() {
+		return statetype;
+	}
+	public void setStatetype(StateType statetype) {
+		this.statetype = statetype;
+	}
 	
 	
 	
