f9cfda2d9e44ab98723f5b64eb0b9b7e0f3d835a
özge <zge@Lenovo-PC>
2018-06-20 11:45:07 +0300

    jdhlkd

---
+++ src/main/java/com/project/yasar/onduty/onduty/repository/PersonalRepositoryImpl.java
@@ -0,0 +1,79 @@
+package com.project.yasar.onduty.onduty.repository;
+
+import com.project.yasar.onduty.onduty.domain.Personal;
+
+public class PersonalRepositoryImpl implements PersonalRepository {
+
+	@Override
+	public <S extends Personal> S save(S entity) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <S extends Personal> Iterable<S> save(Iterable<S> entities) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Personal findOne(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean exists(Long id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Iterable<Personal> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterable<Personal> findAll(Iterable<Long> ids) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long count() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(Personal entity) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(Iterable<? extends Personal> entities) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void deleteAll() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Personal findPersonalByPersonalId(String personalid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
